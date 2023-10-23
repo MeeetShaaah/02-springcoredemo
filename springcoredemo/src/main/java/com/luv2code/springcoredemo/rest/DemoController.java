e0810fa965c08da85455fe9538929723a2594631
Meet Shah <meetshah7199@gmail.com>
2023-10-23 11:29:43 -0400

    Component Scanning

--- springcoredemo/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ springcoredemo/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -1,8 +1,10 @@
-package com.luv2code.springcoredemo;
+package com.luv2code.springcoredemo.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.luv2code.springcoredemo.common.Coach;
 
 @RestController
 public class DemoController {
